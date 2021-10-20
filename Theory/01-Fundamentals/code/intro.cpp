7bf24c50747a913f121da45e2877bc40986cac65
Pranay Gupta <pranayguptamain@gmail.com>
2021-10-20 13:10:51 +0530

    Create intro.cpp

---
+++ Theory/01-Fundamentals/code/intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num, balence = 50000, updated_balence;
+    cout << "Enter How Much Amount You Want : ";
+    cin >> num;
+    updated_balence = balence - num; // Balence Gets Updated here
+    cout << "Updated Amount is : " << updated_balence;
+    /* The
+    Program
+    Ends
+    Here*/
+    return 0;
+}
