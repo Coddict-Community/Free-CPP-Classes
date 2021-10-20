03cdd5ddb150d4eaf66efd42dbc64b802b68eff1
Pranay Gupta <pranayguptamain@gmail.com>
2021-10-20 13:11:19 +0530

    Create validVar.cpp

---
+++ Theory/01-Fundamentals/code/validVar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1. Variable Name Should not Start With Number.
+    // 2.Variable Can Start with Character underscore(_).
+    // 3. Varible Name Cannot Consist of any Space or Special Character.
+    // 4. Variable Name Cannot be any Keyword.
+
+    int sum1=10;
+    int Sum1=20;                          
+
+    cout << "\\Hello\n";
+    cout << "\nI am\t a \'Quotation\"";
+    cin>>sum1;
+    cin>>Sum1;
+    cout<<"sum1 : "<<sum1<<endl;
+    cout<<"Sum1 : "<<Sum1<<endl;
+    return 0;
+}
