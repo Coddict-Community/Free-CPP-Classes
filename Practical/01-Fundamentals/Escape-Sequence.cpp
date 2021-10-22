8da5b55bbf8536da7ca70b5d997c077094df34f5
Abir Pal <74393266+Abirpal202049@users.noreply.github.com>
2021-10-22 23:20:50 +0530

    Update Escape-Sequence.cpp

--- Practical/01-Fundamentals/Escape-Sequence.cpp
+++ Practical/01-Fundamentals/Escape-Sequence.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout << "This\nis\na\ntest\n\nShe said, \"Sells she seashells on the seashore?\"\n";
+    // '\n' and 'endl' both has same operation
+    cout << "This\nis\na\ntest\n\nShe said, \"Sells she seashells on the seashore?\"\n"; // \n moves the cursor to the next line
+    cout << "Coddict\rCommunity"; // \r moves the cursor to the start of the line
+    cout << "Coddict\b"; // \b add a backspace after printing
+    cout << "Coddict\tCommunity"; //\t adds a tab 
 
 /*
     This
