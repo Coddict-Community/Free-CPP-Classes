ce65bc6c42fae9a5e06d75ee8f721a631651ed70
Pranay Gupta <pranayguptamain@gmail.com>
2021-10-29 20:40:31 +0530

    practical work on 29-10-2021

---
+++ Practical/02-Conditional-Statements/Switch-Case2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int marks;
+  cout<<"Enter the marks: ";
+  cin>>marks;
+  
+  switch(marks){
+    case 0 ... 39:
+    cout<<"Fail\n";
+    break;
+    
+    case 40 ... 49:
+    cout<<"D\n";
+    break;
+    
+    case 50 ... 59:
+    cout<<"C\n";
+    break;
+    
+    case 60 ... 69:
+    cout<<"B\n";
+    break;
+    
+    case 70 ... 79:
+    cout<<"A\n";
+    break;
+    
+    case 80 ... 89:
+    cout<<"E\n";
+    break;
+    
+    case 90 ... 100:
+    cout<<"O\n";
+    break;
+    
+    default:
+    cout<<"Enter Valid mark.\n";
+    break;
+  }
+}
