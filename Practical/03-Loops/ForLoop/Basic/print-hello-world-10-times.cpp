0bae45dd3bb55ff731e900802c121b91d84e6aec
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2021-11-05 21:21:31 +0530

    Create print-hello-world-10-times.cpp

---
+++ Practical/03-Loops/ForLoop/Basic/print-hello-world-10-times.cpp
@@ -0,0 +1,11 @@
+//printing hello world 10 times
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int i = 0; 
+   for(i = 0 ; i < 10 ; i ++){
+     cout<<"Hello World"<<endl;
+   }
+}
